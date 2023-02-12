33db8f73c727067b7fa097121a8c40cc78bb5af0
alina-vakulenko <alina.vakulenko12@gmail.com>
2023-02-12 22:15:41 +0200

    added bar and pie charts

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import Invoices from "./scenes/invoices/Invoices";
 import Form from "./scenes/form/Form";
 import Calendar from "./scenes/calendar/Calendar";
 import FAQ from "./scenes/faq/Faq";
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar/Bar";
+import Pie from "./scenes/pie/Pie";
 // import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
 // import Geography from "./scenes/geography";
 
 function App() {
@@ -33,8 +33,8 @@ function App() {
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/faq" element={<FAQ />} />
-              {/* <Route path="/bar" element={<Bar />} /> */}
-              {/* <Route path="/pie" element={<Pie />} /> */}
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
